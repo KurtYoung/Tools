3a152f299e47a7a8bb0d6e4bb1d420e148642118
Kurt Young <ykt836@gmail.com>
2012-03-22 10:49:59 +0800

    add template.cpp

---
+++ acm/template.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <set>
+using namespace std;
+
+#define FOR(i, n) for (int i = 0; i < (n); ++i)
+#define FOR(i, b, n) for (int i = (b); i < (n); ++i)
+
+template<class T> void output(T _array, int _n) {
+    FOR(_i, _n) cout << _array[_i] << " "; cout << endl;
+}
+template<class T> void output(T _array, int _n, int _m) {
+    FOR(_i, _n) output(_array[_i], _m);
+}
+
+int main() {
+#ifdef DEBUG
+    freopen("input", "r", stdin);
+#endif
+
+    return 0;
+}
